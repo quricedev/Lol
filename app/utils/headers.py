308ca2f210f37f3494e08f085bb499ba186f8037
quricedev <pvishwas382@gmail.com>
2026-04-14 20:30:12 +0530

    Add headers function to return HTTP headers

---
+++ app/utils/headers.py
@@ -0,0 +1,6 @@
+def headers():
+    return {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
